01a71af6939965f8f98e5facc1077b46401960fa
Thomas George <private@thomas-george.com>
2022-03-19 09:43:58 +0100

    Initial Commit

---
+++ ToDo-MVVM/ToDo_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_MVVMApp.swift
+//  ToDo-MVVM
+//
+//  Created by Thomas George on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
